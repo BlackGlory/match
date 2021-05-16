8b8e187b2b4abef654c5f64da1fceac8e2720828
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-05-16 19:11:43 +0000

    test: fix

--- __tests__/utils.ts
+++ __tests__/utils.ts
@@ -4,7 +4,9 @@ import { nextSibling } from 'extra-dom'
 export function createContext(): IReadonlyContext {
   return {
     document
-  , next: nextSibling
+  , next(node: Node, distance?: number): Node | null {
+      return nextSibling(node, distance) ?? null
+    }
   , collection: {}
   }
 }

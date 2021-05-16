b159511933cd0115f7008880d41c78c7500a2974
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-05-16 19:06:25 +0000

    chore: upgrade dependencies

--- src/utils/next-element-sibling.ts
+++ src/utils/next-element-sibling.ts
@@ -3,5 +3,5 @@ import * as ExtraDOM from 'extra-dom'
 export function nextElementSibling(node: Node, distance: number = 1): Node | null {
   if (distance === 0) return node
 
-  return ExtraDOM.nextElementSibling(node, distance)
+  return ExtraDOM.nextElementSibling(node, distance) ?? null
 }
--- src/utils/next-sibling.ts
+++ src/utils/next-sibling.ts
@@ -3,5 +3,5 @@ import * as ExtraDOM from 'extra-dom'
 export function nextSibling(node: Node, distance: number = 1): Node | null {
   if (distance === 0) return node
 
-  return ExtraDOM.nextSibling(node, distance)
+  return ExtraDOM.nextSibling(node, distance) ?? null
 }
